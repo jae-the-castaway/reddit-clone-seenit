73fa0283328bedbbcbfe10b2289596e419864b46
Jaeheon Kim <jaeheonkim95@gmail.com>
2023-01-26 14:28:58 -0600

    build set up with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' text-red-800 '>hello there</div>
+    </>
+  )
+}
+
+export default App
